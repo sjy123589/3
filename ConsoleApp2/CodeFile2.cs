14ed5fdb2961591a2f3d5d58a90c75e219346c9d
sjy123589 <3186430344@qq.com>
2022-09-05 19:42:46 +0800

    22222

---
+++ ConsoleApp2/CodeFile2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CodeFile2
+{
+    class CodeFile2
+    {
+        static void Main(string[] args)
+        {
+            short a;
+            int b;
+            double c;
+
+            //初始化变量a
+            a = 10;
+            //读取键盘输入，并转换为整数
+            Console.WriteLine("请输入一个数字：");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            //输出
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.ReadLine();
+        }
+    }
+}
